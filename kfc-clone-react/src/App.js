176694d8299dc2a002460e2ca5a0adeb163ce835
SuryatejaParvata <96101598+SuryatejaParvata@users.noreply.github.com>
2022-05-07 11:57:02 +0530

    deals and about page

---
+++ kfc-clone-react/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+// import './App.css';
+
+
+import { Deals } from './components/Deals/Deals';
+import { About } from './components/About/About';
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <About/>
+    </div>
+    </>
+  );
+}
+
+export default App;
