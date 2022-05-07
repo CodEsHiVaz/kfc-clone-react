dc45783ac60c21d67155fd8c8e4adef1c205be09
SuryatejaParvata <96101598+SuryatejaParvata@users.noreply.github.com>
2022-05-07 12:21:19 +0530

    added surya components  folder

---
+++ kfc-react-app/src/components/surya_ components/About/About.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import "./about.css";
+export const About = () => {
+  return (
+    <div>
+      <div className="blackstrip">
+        <p>OUR GOLDEN PAST</p>
+        <p>KYC&nbsp;(KNOW YOUR CHICKEN)</p>
+        <p>KFC CARE</p>
+        <p>KFC FEEDBACK</p>
+        <p>CAREERS</p>
+      </div>
+      <div className="welcomenote">
+        <div className="redcolor">
+          <h1>Welcome</h1> <br />
+          <h1>
+            <strong>CHICKEN LOVERS</strong>
+          </h1>
+        </div>
+        <div className="bannerimg"></div>
+      </div>
+
+      <div className="imgcards">
+        <div>
+          <h1 className="child">OUR CRISPY HERITAGE</h1>
+        </div>
+        <div>
+          <h1 className="child">ALL ABOUT CHICKEN</h1>
+        </div>
+        <div>
+          <h1 className="child">BEING FAST FOOD</h1>
+        </div>
+        <div>
+          <h1 className="child">JOIN THE COLONEL'S CREW</h1>
+        </div>
+      </div>
+
+      <div className="journey">
+        <div className="journeytext">
+          <h1>Journey of fried chicken, through the ages</h1>
+          <p>
+            It all started with Colonel Harland Sanders creating a finger
+            lickin’ good recipe more than 75 years ago. A list of secret herbs
+            and spices scratched out on the back of his kitchen <br />
+            door, that continues to be followed across 145 countries, with more
+            than 800,000 team members breading and freshly preparing bucket
+            after bucket of our signature Hot & <br />
+            Crispy chicken. The Colonel’s Secret Recipe continues to be as
+            guarded; and our commitment towards Food, People and Planet remains
+            as steadfast. With a new KFC restaurant <br />
+            opening every 6 hours, we power on to serve the World’s Tastiest
+            Chicken!
+          </p>
+        </div>
+        <div className="demographics">
+          <div className="demoinfo">
+            <img
+              src="https://about.kfc.co.in/assets/images/D/Icons/calendar.png"
+              alt=""
+            />
+            <p>Since</p>
+            <h2>1930</h2>
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAAeCAYAAAB5c901AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABNSURBVHgB7c9BDQAgAMPAgQ4c4F8KDvABFuBHSe+9LGmZ6SsXWkY52b3yW/M5A+kMpDOQzkA6A+kMpDOQzkA6A+kMpDOQzkA6A+kMpNu9MQhKmC+cDgAAAABJRU5ErkJggg==" alt="" />
+          </div>
+          <div className="demoinfo">
+          <img
+              src="https://about.kfc.co.in/assets/images/D/Icons/country.png"
+              alt=""
+            />
+            <p>Countries</p>
+            <h2>145</h2>
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAAeCAYAAAB5c901AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABNSURBVHgB7c9BDQAgAMPAgQ4c4F8KDvABFuBHSe+9LGmZ6SsXWkY52b3yW/M5A+kMpDOQzkA6A+kMpDOQzkA6A+kMpDOQzkA6A+kMpNu9MQhKmC+cDgAAAABJRU5ErkJggg==" alt="" />
+          </div>
+          <div className="demoinfo">
+          <img
+              src="https://about.kfc.co.in/assets/images/D/Icons/kfc-board.png"
+              alt=""
+            />
+            <p>Restaurants</p>
+            <h2>25k+</h2>
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAAeCAYAAAB5c901AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABNSURBVHgB7c9BDQAgAMPAgQ4c4F8KDvABFuBHSe+9LGmZ6SsXWkY52b3yW/M5A+kMpDOQzkA6A+kMpDOQzkA6A+kMpDOQzkA6A+kMpNu9MQhKmC+cDgAAAABJRU5ErkJggg==" alt="" />
+          </div>
+          <div className="demoinfo">
+          <img
+              src="https://about.kfc.co.in/assets/images/D/Icons/kitchen-lovers.png"
+              alt=""
+            />
+            <p>Team Members</p>
+            <h2>800k+</h2>
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAAeCAYAAAB5c901AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABNSURBVHgB7c9BDQAgAMPAgQ4c4F8KDvABFuBHSe+9LGmZ6SsXWkY52b3yW/M5A+kMpDOQzkA6A+kMpDOQzkA6A+kMpDOQzkA6A+kMpNu9MQhKmC+cDgAAAABJRU5ErkJggg==" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ kfc-react-app/src/components/surya_ components/Deals/Deals.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./deals.css";
+export const Deals = () => {
+  return (
+    <div className="address">
+      <div className="banner">
+      <div class="deals-image-text">Deals &amp; Offers</div>
+      </div>
+
+      <div className="stripes">
+        <img
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAAeCAYAAAB5c901AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABNSURBVHgB7c9BDQAgAMPAgQ4c4F8KDvABFuBHSe+9LGmZ6SsXWkY52b3yW/M5A+kMpDOQzkA6A+kMpDOQzkA6A+kMpDOQzkA6A+kMpNu9MQhKmC+cDgAAAABJRU5ErkJggg=="
+          alt=""
+        />
+      </div>
+      <h1 className="offertitle">OFFERS FOR YOU</h1>
+      <div className="hotncrispy">
+        <img
+          src="https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/offers/xl/CHKZINGER.jpg"
+          className="offers-menu-product-image"
+        />
+        <h4 className="offers-deals-product-title">1 Pc free Chicken... </h4>
+        <h5 className="offers-deals-product-desc">
+          1 Pc free Chicken Zinger on a cart value of 399 or above on first
+          order. Only for registered users.
+        </h5>
+        <div className="hotncrispydiv">
+          <button className="view-details-btn">View Details</button>
+          <button className="redeem-btn">Redeem</button>
+        </div>
+      </div>
+    </div>
+  );
+};
