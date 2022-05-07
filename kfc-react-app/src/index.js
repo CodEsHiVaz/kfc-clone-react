a614ff9c86d784a4c31dff41afa6a353322f7a56
Santanu55555 <99542723+Santanu55555@users.noreply.github.com>
2022-05-07 20:33:34 +0530

    ok

--- kfc-react-app/src/index.js
+++ kfc-react-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { createRoot } from "react-dom/client";
 import {BrowserRouter} from "react-router-dom"
 import {Provider} from "react-redux"
